ea2090e2275e794b114c68e6030322a95bf1291b
nirbhay-iota <nirbhay200317@gmail.com>
2025-10-03 10:45:50 +0530

    Update longestSubarray.cpp

--- Arrays/longestSubarray.cpp
+++ Arrays/longestSubarray.cpp
@@ -33,6 +33,11 @@ void longest_subarray_brute_better(vector<int> arr,int n,int k){
     }
     cout<<"Longest subarray with sum k using brute better approach has length: "<<longest<<endl;
 }
+//preSumMap.end() means not found....{.end() gives the element which is one past the last element}
+//time comp=O(n*logn)... n for the for loop and logn for finding in the hashmap
+//time complexity can be reduced to O(n*1)..if we use an unordered map but in the worst with collisions
+//..an unordered map finding time is n so in the worst case it can become O(n^2)...that's why we use ordered map with worst case TC=O(nlogn)
+//space comp=O(n)-->for the hashmap
 void longest_subarray_better(vector<int> arr,int n,long long k){
     map<long long,int> preSumMap;
     int longest=0;
@@ -40,16 +45,43 @@ void longest_subarray_better(vector<int> arr,int n,long long k){
     for(int i=0;i<n;i++){
         sum+=arr[i];
         if(sum==k){
-            longest=max(longest,i+1);     //this if is only for the condition when first element=k
-        }
-        long long remaining=sum-k;
-        if(preSumMap.find(remaining) != preSumMap.end()){
-            int length=i-preSumMap[remaining];    //this gives the length of subarray...preSumMap[remaining] gives value at key remaining
-            
+            longest=max(longest,i+1);     //this is for the condition if the starting elements make up to k
+                                          //ex...there was 1,1,1 in the starting 
         }
 
+        long long remaining=sum-k;
+        //read point 4 of readme once
+        //So this if statement returns true if find(remaining) is no equal to not found...i.e. it is present in map
+        if(preSumMap.find(remaining) != preSumMap.end()){
+            int length=i-preSumMap[remaining];    //this gives the length of subarray...preSumMap[remaining] gives value at key remaining
+            longest=max(longest,length);
+        }
+
+        if(preSumMap.find(sum)==preSumMap.end()){
+            preSumMap[sum]=i;
+        }
     }
+    cout<<"Longest subarray with sum k using better approach has length: "<<longest<<endl;
 }
+//We will apply kind of a 2 pointer and greedy approach
+//time comp=O(n)
+//space comp=O(1)
+void longest_subarray_optimal(vector<int> arr,int n,long long k){
+    int j=0,sum=0,longest=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum>k){
+            sum=sum-arr[j];
+            j++;
+        }
+        if(sum==k){
+            int length=i-j+1;
+            longest=max(longest,length);
+        }
+    }
+    cout<<"Longest subarray with sum k using optimal approach has length: "<<longest<<endl;
+}
+
 int main(){
     int n;
     cin>>n;
@@ -67,5 +99,6 @@ int main(){
     cout<<endl;
     longest_subarray_brute(arr,n,k);
     longest_subarray_brute_better(arr,n,k);
-    cout<<endl;
+    longest_subarray_better(arr,n,k);
+    longest_subarray_optimal(arr,n,k);}
 }
