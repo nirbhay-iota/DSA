bb5763c5571b3d9b62f76bf1a1d55791b8cbe99c
nirbhay-iota <nirbhay200317@gmail.com>
2025-11-27 13:24:59 +0530

    Add files via upload

---
+++ Binary Search/rotatedArray1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void index_of_target(vector<int> nums,int target){
+    int low=0,high=nums.size()-1;
+    int ans=-1;
+    while (high>=low){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target){
+            cout<<"The index of target is:"<<mid<<endl;
+            return;
+        }
+        //either of the sides of mid will be sorted
+        // (If left sorted)
+        if(nums[low]<=nums[mid]){
+            if(target<=nums[mid] && target>=nums[low]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        //(If right sorted)
+        else{
+            if(target>=nums[mid] && nums[high]>=target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        ans=mid;
+    }
+    cout<<"The index of target is:"<<ans<<endl;
+}
+int main(){
+    int n; cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<"Input Array:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    int target;
+    cout<<"Enter Target"<<endl;
+    cin>>target;
+    index_of_target(nums,target);
+}
