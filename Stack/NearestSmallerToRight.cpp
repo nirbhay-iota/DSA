f867abf0c53c7c1c53f68de328d4579dd7c3e839
nirbhay-iota <nirbhay200317@gmail.com>
2026-02-20 12:59:25 +0530

    Add Nearest Smaller to Right functionality
    Implement a function to find the next smaller element for each element in the array.

---
+++ Stack/NearestSmallerToRight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> nextSmallerEle(vector<int>& arr) {
+        
+        stack<int> st;
+        vector<int > ans;
+        for(int i=arr.size()-1;i>=0;i--){
+            if(st.empty())  ans.push_back(-1);
+            else if(st.size()>0 && st.top()<arr[i]){
+                ans.push_back(st.top());
+            }
+            else if(st.size()>0 && st.top()>=arr[i]){
+                while(st.size()>0 && st.top()>=arr[i]){
+                    st.pop();
+                }
+                if(st.empty()) ans.push_back(-1);
+                else ans.push_back(st.top());
+            }
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    
+    
+    }
+};
