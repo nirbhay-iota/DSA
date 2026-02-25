191e757f1e676945a85b933137c3cfb58d9a2a81
nirbhay-iota <nirbhay200317@gmail.com>
2026-02-25 22:22:01 +0530

    Create RainWaterTrapPreSuf.cpp

---
+++ Stack/RainWaterTrapPreSuf.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<int> prefix(height.size(),0);
+        vector<int> suffix(height.size(),0);
+        int ans=0;
+        int largest=0;
+        for(int i=0;i<height.size();i++){
+            largest=max(largest,height[i]);
+            prefix[i]=largest;
+        }
+        largest=0;
+        for(int i=height.size()-1;i>=0;i--){
+            largest=max(largest,height[i]);
+            suffix[i]=largest;
+        }
+        for(int i=0;i<height.size();i++){
+            if(prefix[i]>=suffix[i]){
+                ans=(prefix[i]-suffix[i])*height[i]
+            }
+        }
+    }
+};
