e5da8587bbf17978155382f2e9c5fb054bc0e5b7
nirbhay-iota <nirbhay200317@gmail.com>
2026-02-25 22:32:21 +0530

    Add comments to clarify water trapping logic
    Added comments explaining the logic of the water trapping algorithm.

--- Stack/RainWaterTrapPreSuf.cpp
+++ Stack/RainWaterTrapPreSuf.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        /*
+        Logic last loop me :-
+        => dekh logic ye h ki hm ek point se dusre point pe hoop kar rhe aur hm ab tk jo largest
+        height mila h usse naap rhe h ki paani store hoga ki nhi jiske liye hm prefix use kar rhe hn
+        Ab har point se future me two possible cases h :-
+        1. agar hm ek point pe hn toh aage hmko usse jyda milega
+        2. yaa nhi milega iske liye hm suffix use kar rhe
+        */
         vector<int> prefix(height.size(),0);
         vector<int> suffix(height.size(),0);
         int ans=0;
