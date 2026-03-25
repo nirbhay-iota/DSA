d25f0e9ad2a6ba5c0b24a821b347557b9c2f8ed5
nirbhay-iota <nirbhay200317@gmail.com>
2026-03-25 23:51:47 +0530

    Add cycle length detection using slow and fast pointers
    Implemented a new method to find the length of a cycle in a linked list using the slow and fast pointer technique.

--- Linked Lists/lengthOfCycle.cpp
+++ Linked Lists/lengthOfCycle.cpp
@@ -1,4 +1,4 @@
-
+// using map
 class Solution {
   public:
     int lengthOfLoop(Node *head) {
@@ -15,6 +15,31 @@ class Solution {
             mp[temp]=count;
             temp=temp->next;
             count++;
+        }
+        return 0;
+    }
+};
+ 
+// using slow fast 
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node * slow=head;
+        Node * fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                int count=1;
+                slow=slow->next;
+                while(slow!=fast){
+                    count++;
+                    slow=slow->next;
+                }
+                return count;
+            }
         }
         return 0;
     }
